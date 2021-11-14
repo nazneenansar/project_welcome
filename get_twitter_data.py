8d801778513f6cf9d0767ac9b315269c6490d1b9
AlkaSeltzers <32341871+AlkaSeltzers@users.noreply.github.com>
2021-11-14 10:59:32 -0500

    Add files via upload

---
+++ get_twitter_data.py
@@ -0,0 +1,36 @@
+from config import *
+import tweepy
+import datetime
+import csv
+print("hi")
+auth = tweepy.OAuthHandler("key", "secret")
+auth.set_access_token("key", "secret")
+api = tweepy.API(auth, wait_on_rate_limit=True)
+today = datetime.date.today()
+yesterday= today - datetime.timedelta(days=30)
+print("tweet list")
+tweets_list = tweepy.Cursor(api.search_tweets, q="girlsintech", lang="en", since=yesterday, tweet_mode='extended').items()
+output = []
+csv_file = open("csv_filetechwomen.csv", "w")
+writer = csv.writer(csv_file)
+print("tweet list len")
+for tweet in tweets_list:
+     if tweet.user.verified:
+        continue
+    text1 = tweet.full_text
+    id1 = tweet.id
+    if tweet.retweet_count < 5:
+        continue
+    if len(text1) > 2 and text1[:2] == "RT":
+        print("smh")
+        continue
+
+    print(text1)
+    line = {'text': text1}
+    output.append(line)
+    writer.writerow([text1, id1])
+print("after later")
+import pandas as pd
+df = pd.DataFrame(output)
+df.to_csv('output.csv')
+print("done!")
